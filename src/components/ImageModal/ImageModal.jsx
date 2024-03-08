1978c0832588a7628d1a9184aecc1ab29e7362c8
Oleksandr Lutsenko <sashalutsenko1991@gmail.com>
2024-03-08 11:33:20 +0100

    a

--- src/components/ImageModal/ImageModal.jsx
+++ src/components/ImageModal/ImageModal.jsx
@@ -1,5 +1,5 @@
 import Modal from 'react-modal';
-import { IoMdHeartEmpty } from 'react-icons/io';
+import { BiSolidLike } from "react-icons/bi";
 import style from './ImageModal.module.css';
 
 Modal.setAppElement('#root');
@@ -13,31 +13,26 @@ const customStyles = {
     marginRight: '-50%',
     transform: 'translate(-50%, -50%)',
     border: 'none',
-    padding: 32,
+    padding: 25,
   },
 };
 
-const ImageModal = ({
-  closeModal,
-  modalIsOpen,
-  modal: { user, likes, description, alt_description, imgUrl },
-}) => {
+const ImageModal = ({modal: { user, likes, description, alt_description, imgUrl }, closeModal, modalIsOpen}) => {
   return (
-    <Modal isOpen={modalIsOpen} onRequestClose={closeModal} style={customStyles}>
-      <div className={style.imageWrapper}>
+    <Modal style={customStyles} isOpen={modalIsOpen} onRequestClose={closeModal} >
+      <div className={style.wrapper}>
         <img className={style.image} src={imgUrl} alt={alt_description} />
       </div>
-      <div className={style.thumbInfo}>
+      <div className={style.thumb}>
         <div className={style.user}>
           <img
-            className={style.avatar}
             src={user?.profile_image.small}
             alt={alt_description ?? 'Unrecognized image'}
           />
           <span>@{user?.username}</span>
         </div>
         <div className={style.likes}>
-          <IoMdHeartEmpty size={20} />
+          <BiSolidLike size={20} />
           <span>{likes}</span>
         </div>
       </div>
